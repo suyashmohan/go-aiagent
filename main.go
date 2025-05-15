45fab2418705c6f7379a98e2d57364e3782d9001
Suyash Mohan <suyashatweb@gmail.com>
2025-05-16 00:40:15 +0800

    minor changes

--- main.go
+++ main.go
@@ -70,39 +70,41 @@ func getTools(mcpClients map[string]client.MCPClient) map[string]internal.AgentT
 		toolsResult, err := mcpClient.ListTools(context.Background(), toolsRequest)
 		if err != nil {
 			log.Printf("Failed to list tools: %v", err)
-		} else {
-			log.Printf("%s has %d tools available\n", mcpName, len(toolsResult.Tools))
-			for i, tool := range toolsResult.Tools {
-				log.Printf("%d. %s - %s\n", i+1, tool.Name, tool.Description)
-				toolName := fmt.Sprintf("%s__%s", mcpName, tool.Name)
-				tools[toolName] = internal.AgentTool{
-					Description: tool.Description,
-					Parameters:  tool.InputSchema.Properties,
-					Required:    tool.InputSchema.Required,
-					Fn: func(m map[string]interface{}) string {
-						log.Println("Inside", toolName, m)
-						callToolReq := mcp.CallToolRequest{}
-						callToolReq.Params.Name = tool.Name
-						callToolReq.Params.Arguments = m
-						callToolRes, err := mcpClient.CallTool(context.Background(), callToolReq)
-						if err != nil {
-							log.Printf("Failed to call tool: %v", err)
-						}
-						// Extract text content
-						var resultText string
-						// Handle array content directly since we know it's []interface{}
-						for _, item := range (*callToolRes).Content {
-							if contentMap, ok := item.(mcp.TextContent); ok {
-								resultText += fmt.Sprintf("%v ", contentMap.Text)
-							}
-						}
-						log.Println(resultText)
+			return nil
+		}
 
-						return resultText
-					},
-				}
+		log.Printf("%s has %d tools available\n", mcpName, len(toolsResult.Tools))
+		for i, tool := range toolsResult.Tools {
+			log.Printf("%d. %s - %s\n", i+1, tool.Name, tool.Description)
+			toolName := fmt.Sprintf("%s__%s", mcpName, tool.Name)
+			tools[toolName] = internal.AgentTool{
+				Description: tool.Description,
+				Parameters:  tool.InputSchema.Properties,
+				Required:    tool.InputSchema.Required,
+				Fn: func(m map[string]interface{}) string {
+					log.Println("Inside", toolName, m)
+					callToolReq := mcp.CallToolRequest{}
+					callToolReq.Params.Name = tool.Name
+					callToolReq.Params.Arguments = m
+					callToolRes, err := mcpClient.CallTool(context.Background(), callToolReq)
+					if err != nil {
+						log.Printf("Failed to call tool: %v", err)
+					}
+					// Extract text content
+					var resultText string
+					// Handle array content directly since we know it's []interface{}
+					for _, item := range (*callToolRes).Content {
+						if contentMap, ok := item.(mcp.TextContent); ok {
+							resultText += fmt.Sprintf("%v ", contentMap.Text)
+						}
+					}
+					log.Println(resultText)
+
+					return resultText
+				},
 			}
 		}
+
 	}
 
 	return tools
